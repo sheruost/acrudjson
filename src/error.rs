07df77003f493495bdd25e74d51108aec8f5a2f2
Sheldon Knuth <sheldon@abackbone.net>
2023-09-29 17:08:34 +0800

    add expect database value error and improve error messages.

--- src/error.rs
+++ src/error.rs
@@ -26,15 +26,21 @@ pub enum ServerError {
     ParseParamNumeric(#[from] bigdecimal::ParseBigDecimalError),
     #[error("missing {0} parameter")]
     MissingParam(usize),
+    #[error("the parameter at index {0} must be a name.")]
+    MissingName(usize),
+    #[error("the parameter at index {0} must be decimal number.")]
+    MissingNumber(usize),
     #[error("`{0}` is not found in user database")]
     DbKeyNotFound(Box<str>),
+    #[error("key [\"{0}\"] does not hold any value.")]
+    DbEmptyValue(Box<str>),
     #[error("`{0}` does not exist so it cannot be updated from user database")]
     DbKeyUpdate(Box<str>),
     #[error(transparent)]
     Io(#[from] std::io::Error),
-    #[error("failed to update value from user database, reason: {0}")]
+    #[error("failed to create new key-value pair, the key entry is already existed.")]
     SledCas(#[from] sled::CompareAndSwapError),
-    #[error("user database error, reason: {0}")]
+    #[error("ACID transaction error from user database, reason: {0}")]
     SledInternal(#[from] sled::Error),
     #[error("value error, expect: {expect}, actual: {actual}")]
     ValueError { expect: Box<str>, actual: Box<str> },
@@ -69,16 +75,21 @@ impl From<ServerError> for ErrorMsg {
                 ErrorMsg("failed to parse paramater into floating number.".to_string())
             }
             ServerError::MissingParam(count) => ErrorMsg(format!("missing {count} parameter.")),
-            ServerError::DbKeyNotFound(key) => ErrorMsg(format!("`{key}` not found.")),
-            ServerError::DbKeyUpdate(key) => ErrorMsg(format!("`{key}` does not exist.")),
+            ServerError::MissingName(idx) => ErrorMsg(format!("index {idx} must be a name.")),
+            ServerError::MissingNumber(idx) => {
+                ErrorMsg(format!("index {idx} must be decimal number."))
+            }
+            ServerError::DbKeyNotFound(key) => ErrorMsg(format!("[\"{key}\"] not found.")),
+            ServerError::DbEmptyValue(key) => ErrorMsg(format!("[\"{key}\"] has empty value.")),
+            ServerError::DbKeyUpdate(key) => ErrorMsg(format!("[\"{key}\"] does not exist.")),
             ServerError::SledCas(_) => {
-                ErrorMsg("failed to create new value in user database.".to_string())
+                ErrorMsg("failed to create new value in user database".to_string())
             }
             ServerError::SledInternal(_) => {
                 ErrorMsg("failed to fetch or update value in user database.".to_string())
             }
             ServerError::ValueError { .. } => {
-                ErrorMsg("failed to fetch number from JSON request \"params\" field.".to_string())
+                ErrorMsg("failed to parse decimal number by requesting name.".to_string())
             }
             _ => ErrorMsg("internal I/O error.".to_string()),
         }
